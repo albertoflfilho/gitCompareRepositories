a66fb4693258ab58c7ccdff0a1c5ed0619d3d0c3
Alberto Lacerda <albertoflfilho@gmail.com>
2019-01-22 15:36:06 -0500

    seding repositories b props

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -2,110 +2,44 @@ import React from 'react';
 
 import { Container, Repository } from './styles';
 
-const CompareList = () => (
-  <Container> 
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-
-      <ul>
-        <li>
-          95,019
-          <small>
-            starts
-          </small>
-        </li>
-        <li>
-          95,019
-          <small>
-            forks
-          </small>
-        </li>
-        <li>
-          95,019
-          <small>
-            issues
-          </small>
-        </li>
-        <li>
-          3 days ago
-          <small>
-            last commit
-          </small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-
-      <ul>
-        <li>
-          95,019
-          <small>
-            starts
-          </small>
-        </li>
-        <li>
-          95,019
-          <small>
-            forks
-          </small>
-        </li>
-        <li>
-          95,019
-          <small>
-            issues
-          </small>
-        </li>
-        <li>
-          3 days ago
-          <small>
-            last commit
-          </small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-
-      <ul>
-        <li>
-          95,019
-          <small>
-            starts
-          </small>
-        </li>
-        <li>
-          95,019
-          <small>
-            forks
-          </small>
-        </li>
-        <li>
-          95,019
-          <small>
-            issues
-          </small>
-        </li>
-        <li>
-          3 days ago
-          <small>
-            last commit
-          </small>
-        </li>
-      </ul>
-    </Repository>
+const CompareList = ({ repositories }) => (
+  <Container>
+    { repositories.map(repository => (
+          <Repository>
+            <header>
+              <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+              <strong>{repository.name}</strong>
+              <small>{repository.owner.login}</small>
+            </header>
+      
+            <ul>
+              <li>
+                {repository.stargazers_count}
+                <small>
+                  starts
+                </small>
+              </li>
+              <li>
+              {repository.forks_count}
+                <small>
+                  forks
+                </small>
+              </li>
+              <li>
+              {repository.open_issues_count}
+                <small>
+                  issues
+                </small>
+              </li>
+              <li>
+              {repository.pushed_at}
+                <small>
+                  last commit
+                </small>
+              </li>
+            </ul>
+        </Repository>
+    )) }
   </Container>
 );
 
